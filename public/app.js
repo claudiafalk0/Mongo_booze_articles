a5c0cdeaead8bd8216f1c585a894b97784d8e1a3
Claudia Falk <claudiafalk0@gmail.com>
2019-07-24 18:27:32 -0600

    added mongoose, save files

--- public/app.js
+++ public/app.js
@@ -6,9 +6,9 @@ function displayResults(data) {
             <div class='carousel-caption d-none d-md-block'>
             <h5>${data[i].title}</h5>
             <a href='${data[i].link}'>Link to article</a>
-            <a class="saveArticle" href="/save" role="button" style = 'text-align:center'>
-            <span>Save Article</span>
-            </a>
+            <button type='button' data-id= '${data[i]._id}' class="btn btn-primary" data-toggle='modal' data-target='noteModal' style = 'text-align:center'>
+            Make Notes
+            </button>
             </div>
             </div>`
     )};
@@ -19,3 +19,65 @@ function displayResults(data) {
 $.getJSON('/', function (data) {
     displayResults(data)
 });
+
+$(document).on("click", "button", function() {
+    // Empty the notes from the note section
+    $("#notesModal").empty();
+    // Save the id from the p tag
+    var thisId = $(this).attr("data-id");
+  
+    // Now make an ajax call for the Article
+    $.ajax({
+      method: "GET",
+      url: "/articles/" + thisId
+    })
+      // With that done, add the note information to the page
+      .then(function(data) {
+        console.log(data);
+        // The title of the article
+        $("#notesTitle").append(data.title);
+        // An input to enter a new title
+        $("#newTitle").append();
+        // A textarea to add a new note body
+        $("textarea").append();
+        // A button to submit a new note, with the id of the article saved to it
+        $("#saveChanges").append("<data-id='" + data._id + "'>");
+  
+        // If there's a note in the article
+        if (data.note) {
+          // Place the title of the note in the title input
+          $("#newTitle").val(data.note.title);
+          // Place the body of the note in the body textarea
+          $("textarea").val(data.note.body);
+        }
+      });
+  });
+  
+  // When you click the savenote button
+  $(document).on("click", "#saveChanges", function() {
+    // Grab the id associated with the article from the submit button
+    var thisId = $(this).attr("data-id");
+  
+    // Run a POST request to change the note, using what's entered in the inputs
+    $.ajax({
+      method: "POST",
+      url: "/articles/" + thisId,
+      data: {
+        // Value taken from title input
+        title: $("#newTitle").val(),
+        // Value taken from note textarea
+        body: $("textarea").val()
+      }
+    })
+      // With that done
+      .then(function(data) {
+        // Log the response
+        console.log(data);
+        // Empty the notes section
+        $("#notes").empty();
+      });
+  
+    // Also, remove the values entered in the input and textarea for note entry
+    $("#newTitle").val("");
+    $("textarea").val("");
+  });
--- server.js
+++ server.js
@@ -1,32 +1,33 @@
 var express = require("express");
-var mongojs = require("mongojs");
-// Require axios and cheerio. This makes the scraping possible
+var logger = require("morgan");
+var mongoose = require("mongoose");
+
+// Our scraping tools
+// Axios is a promised-based http library, similar to jQuery's Ajax method
+// It works on the client and on the server
 var axios = require("axios");
 var cheerio = require("cheerio");
+
+// Require all models
+var db = require("./models");
+
+var PORT = 3000;
 
 // Initialize Express
 var app = express();
 
-// Database configuration
-var databaseUrl = "scraper";
-var collections = ["scrapedData"];
+// Configure middleware
 
-// Hook mongojs configuration to the db variable
-var db = mongojs(databaseUrl, collections);
-db.on("error", function(error) {
-  console.log("Database Error:", error);
-});
+// Use morgan logger for logging requests
+app.use(logger("dev"));
+// Parse request body as JSON
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+// Make public a static folder
+app.use(express.static("public"));
 
-
-// Log the results once you've looped through each of the elements found with cheerio
-
-// Main route (simple Hello World Message)
-app.get("/", function(req, res) {
-  db.scrapedData.find({}, function(err,data){
-      if(err) throw err;
-      res.json(data);
-  });
-});
+// Connect to the Mongo DB
+mongoose.connect("mongodb://localhost/unit18Populater", { useNewUrlParser: true });
 
 // TODO: make two more routes
 
@@ -35,7 +36,7 @@ app.get("/", function(req, res) {
 // This route will retrieve all of the data
 // from the scrapedData collection as a json (this will be populated
 // by the data you scrape using the next route)
-app.get("/all", function(req, res) {
+app.get("/", function(req, res) {
   // Query: In our database, go to the animals collection, then "find" everything
   db.scrapedData.find({}, function(err, data) {
     // Log any errors if the server encounters one
@@ -64,32 +65,49 @@ app.get('/scrape', function(req, res){
     var $ = cheerio.load(response.data);
     
     $(".post").each(function (i, element) {
-      
+      var result = {}
     //   var title = $(element).find('.overlay').toString().split('/')[2].replace(/-/g, ' ').replace(/\b[a-z]/g, function(chr){
     //       return chr.toUpperCase();
     //   });
     //   var link = $(element).find('a').attr("href");
     //   toString().split('(')[1].split(')')[0].replace(/&apos;/g,"");
-    var title = $(element).find('h3').text().replace(/\s/g,'').replace('Drinks&Spirits', "").replace(/([a-z])([A-Z])/g, '$1 $2');
+    result.title = $(element).find('h3').text().replace(/\s/g,'').replace('Drinks&Spirits', "").replace(/([a-z])([A-Z])/g, '$1 $2');
     // .replace(/\/n/g, "").replace(/\/t/g, "");
-    var link = $(element).find('a').attr("href");
-    var img = $(element).find('.post-thumb > img').attr('src');
+    result.link = $(element).find('a').attr("href");
+    result.image = $(element).find('.post-thumb > img').attr('src');
       
       // Save these results in an object that we'll push into the results array we defined earlier
-      db.scrapedData.insert({
-        title, 
-        link,
-        image: img
-      }, function(err, data){
-        if (err) throw err;
-        console.log(data);
+      db.Article.create(result).then(function(dbArticle){
+        console.log(dbArticle)
+      }).catch(function(err){
+        console.log(err)
       })
-    });
     /* -/-/-/-/-/-/-/-/-/-/-/-/- */
     res.send('Scrape complete');
   });
 });
+});
   
+app.get("/articles/:id", function(req, res) {
+  db.Article.find({_id: req.params.id})
+  .populate('note')
+  .then(function(article){
+    res.json(article)
+  }).catch(function(err){
+    res.json(err)
+  })
+});
+
+app.post("/articles/:id", function(req, res) {
+  db.Note.create(req.body).then(function(note){
+    return db.Article.findOneAndUpdate({_id: (req.params.id)}, {$set: {note: note._id}}, {new: true});
+  }).then(function(dbArticle){
+    res.json(dbArticle);
+  }).catch(function(err){
+    res.json(err);
+  });
+});
+
 
 // Listen on port 3000
 app.listen(3000, function () {
